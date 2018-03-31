f0335bd4029bfaec43b7c4b036935dd133b4be55
Felix Kluge <f.kluge@agdsn.de>
2018-04-01 00:38:12 +0200

    Add fire effect

--- esp32-FastLED-home-assistant/esp32-FastLED-home-assistant.ino
+++ esp32-FastLED-home-assistant/esp32-FastLED-home-assistant.ino
@@ -173,6 +173,152 @@ void reconnect() {
 }
 
 
+bool gReverseDirection = false;
+
+// Fire2012 with programmable Color Palette
+//
+// This code is the same fire simulation as the original "Fire2012",
+// but each heat cell's temperature is translated to color through a FastLED
+// programmable color palette, instead of through the "HeatColor(...)" function.
+//
+// Four different static color palettes are provided here, plus one dynamic one.
+//
+// The three static ones are:
+//   1. the FastLED built-in HeatColors_p -- this is the default, and it looks
+//      pretty much exactly like the original Fire2012.
+//
+//  To use any of the other palettes below, just "uncomment" the corresponding code.
+//
+//   2. a gradient from black to red to yellow to white, which is
+//      visually similar to the HeatColors_p, and helps to illustrate
+//      what the 'heat colors' palette is actually doing,
+//   3. a similar gradient, but in blue colors rather than red ones,
+//      i.e. from black to blue to aqua to white, which results in
+//      an "icy blue" fire effect,
+//   4. a simplified three-step gradient, from black to red to white, just to show
+//      that these gradients need not have four components; two or
+//      three are possible, too, even if they don't look quite as nice for fire.
+//
+// The dynamic palette shows how you can change the basic 'hue' of the
+// color palette every time through the loop, producing "rainbow fire".
+
+CRGBPalette16 gPal;
+
+void setup_fire2012(CRGB basecolor) {
+
+  // This first palette is the basic 'black body radiation' colors,
+  // which run from black to red to bright yellow to white.
+  //gPal = HeatColors_p;
+
+  // These are other ways to set up the color palette for the 'fire'.
+  // First, a gradient from black to red to yellow to white -- similar to HeatColors_p
+  //   gPal = CRGBPalette16( CRGB::Black, CRGB::Red, CRGB::Yellow, CRGB::White);
+
+  // Second, this palette is like the heat colors, but blue/aqua instead of red/yellow
+  //   gPal = CRGBPalette16( CRGB::Black, CRGB::Blue, CRGB::Aqua,  CRGB::White);
+
+  // Third, here's a simpler, three-step gradient, from black to red to white
+  gPal = CRGBPalette16( CRGB::Black, basecolor, CRGB::White);
+}
+
+void loop_fire2012()
+{
+  // Add entropy to random number generator; we use a lot of it.
+  random16_add_entropy(esp_random());
+
+  // Fourth, the most sophisticated: this one sets up a new palette every
+  // time through the loop, based on a hue that changes every time.
+  // The palette is a gradient from black, to a dark color based on the hue,
+  // to a light color based on the hue, to white.
+  //
+  //   static uint8_t hue = 0;
+  //   hue++;
+  //   CRGB darkcolor  = CHSV(hue,255,192); // pure hue, three-quarters brightness
+  //   CRGB lightcolor = CHSV(hue,128,255); // half 'whitened', full brightness
+  //   gPal = CRGBPalette16( CRGB::Black, darkcolor, lightcolor, CRGB::White);
+
+
+  Fire2012WithPalette(); // run simulation frame, using palette colors
+}
+
+
+// Fire2012 by Mark Kriegsman, July 2012
+// as part of "Five Elements" shown here: http://youtu.be/knWiGsmgycY
+////
+// This basic one-dimensional 'fire' simulation works roughly as follows:
+// There's a underlying array of 'heat' cells, that model the temperature
+// at each point along the line.  Every cycle through the simulation,
+// four steps are performed:
+//  1) All cells cool down a little bit, losing heat to the air
+//  2) The heat from each cell drifts 'up' and diffuses a little
+//  3) Sometimes randomly new 'sparks' of heat are added at the bottom
+//  4) The heat from each cell is rendered as a color into the leds array
+//     The heat-to-color mapping uses a black-body radiation approximation.
+//
+// Temperature is in arbitrary units from 0 (cold black) to 255 (white hot).
+//
+// This simulation scales it self a bit depending on NUM_LEDS; it should look
+// "OK" on anywhere from 20 to 100 LEDs without too much tweaking.
+//
+// I recommend running this simulation at anywhere from 30-100 frames per second,
+// meaning an interframe delay of about 10-35 milliseconds.
+//
+// Looks best on a high-density LED setup (60+ pixels/meter).
+//
+//
+// There are two main parameters you can play with to control the look and
+// feel of your fire: COOLING (used in step 1 above), and SPARKING (used
+// in step 3 above).
+//
+// COOLING: How much does the air cool as it rises?
+// Less cooling = taller flames.  More cooling = shorter flames.
+// Default 55, suggested range 20-100
+#define COOLING  70
+
+// SPARKING: What chance (out of 255) is there that a new spark will be lit?
+// Higher chance = more roaring fire.  Lower chance = more flickery fire.
+// Default 120, suggested range 50-200.
+#define SPARKING 140
+
+
+void Fire2012WithPalette()
+{
+// Array of temperature readings at each simulation cell
+  static byte heat[NUM_LEDS];
+
+  // Step 1.  Cool down every cell a little
+    for( int i = 0; i < NUM_LEDS; i++) {
+      heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+    }
+
+    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+    for( int k= NUM_LEDS - 1; k >= 2; k--) {
+      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+    }
+
+    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+    if( random8() < SPARKING ) {
+      int y = random8(7);
+      heat[y] = qadd8( heat[y], random8(160,255) );
+    }
+
+    // Step 4.  Map from heat cells to LED colors
+    for( int j = 0; j < NUM_LEDS; j++) {
+      // Scale the heat value from 0-255 down to 0-240
+      // for best results with color palettes.
+      byte colorindex = scale8( heat[j], 240);
+      CRGB color = ColorFromPalette( gPal, colorindex);
+      int pixelnumber;
+      if( gReverseDirection ) {
+        pixelnumber = (NUM_LEDS-1) - j;
+      } else {
+        pixelnumber = j;
+      }
+      leds[pixelnumber] = color;
+    }
+}
+
+
 void setup() {
   delay(100);
   Serial.begin(115200);
@@ -263,44 +409,32 @@ void ledTask( void * parameter )
     /* check whether receiving is ok or not */
     if(xStatus == pdPASS){
 
-/*
-      Serial.print(data.state);
-      Serial.print(" ");
-      Serial.print(data.hasColor);
-      Serial.print(" ");
-      Serial.print(data.hasBrightness);
-      Serial.print(" ");
-      Serial.println(data.hasEffect);
-*/
+      //effect
+      if(data.hasEffect == 1) {
+        effect = data.effect;
+        Serial.print("Effect changed");
+        Serial.println(effect);
 
-/*
-      StaticJsonBuffer<300> JSONBuffer;                         //Memory pool
-      JsonObject& parsed = JSONBuffer.parseObject(data.payload);
-      free(data.payload);
-
-      //state
-      if(parsed["state"] == "ON")
-        state = true;
-*/
+        if(strcmp(effect, "fire") == 0){
+          setup_fire2012(CRGB::Red);
+        }
+      }
+      
       //color
       if(data.hasColor == 1) {
         oldColor = currentColor;
         newColor = CRGB(data.color_r, data.color_g, data.color_b);
         colorTransition = 0;
+
+        if(strcmp(effect, "fire") == 0){
+          setup_fire2012(newColor);
+        }
       }
       
       //brightness
       if(data.hasBrightness == 1) {
         newBrightness = data.brightness;
       }
-      
-      //effect
-      if(data.hasEffect == 1) {
-        effect = data.effect;
-        Serial.print("Effect changed");
-        Serial.println(effect);
-      }
-
     }
 
     if(colorTransition < 255)
@@ -330,7 +464,10 @@ void ledTask( void * parameter )
       int pos = random16(NUM_LEDS);
       leds[pos] += CHSV( gHue + random8(64), 200, currentBrightness);
     } else if(strcmp(effect,"rainbow") == 0) {
-      fill_rainbow( leds, NUM_LEDS, gHue, 7);
+      fill_rainbow( leds, NUM_LEDS, gHue, 7);  
+    } else if(strcmp(effect,"fire") == 0) {
+      random16_add_entropy(esp_random());
+      loop_fire2012();
     } else {
       for (int i = 0; i < NUM_LEDS; i++) {
           leds[i] = currentColor;
