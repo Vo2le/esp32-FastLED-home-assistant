194ac7b6b12d06c6c192ec2677813eeb74c46863
Felix Kluge <f.kluge@agdsn.de>
2018-04-05 18:50:03 +0200

    Add OTA support

--- esp32-FastLED-home-assistant/esp32-FastLED-home-assistant.ino
+++ esp32-FastLED-home-assistant/esp32-FastLED-home-assistant.ino
@@ -8,7 +8,13 @@
 #include <WiFi.h>
 #include "PubSubClient.h"
 #include "FastLED.h"
+#include <ArduinoOTA.h>
 
+/*
+ * System
+ */
+const char* hostname     = "esp32";
+const char* ota_password = "password";
 
 /*
  * WLAN
@@ -404,6 +410,39 @@ void communicationTask(void * parameter)
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+
+ // OTA Update Stuff
+  ArduinoOTA.setHostname(hostname);
+  ArduinoOTA.setPassword(ota_password);
+ 
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+ 
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
   
   /* keep the status of sending data */
   if (!client.connected()) {
@@ -412,6 +451,7 @@ void communicationTask(void * parameter)
 
   while(1) {
     client.loop();
+    ArduinoOTA.handle();
   }
   
   vTaskDelete( NULL );
